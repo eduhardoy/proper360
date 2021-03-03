53d76ee7896e2be59ef41a97991d181b50c60667
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-03-03 10:07:31 -0300

    Fix Redux

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import adminReducer from "./admin";
 
 const rootReducers = combineReducers({
-  
+  admin: adminReducer,
 });
 
 export default rootReducers;
