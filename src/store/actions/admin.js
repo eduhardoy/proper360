d9640ad24b043ffaa5930f6a48cbdbcd641046b5
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-03-03 09:59:36 -0300

    Redux Thunk

---
+++ src/store/actions/admin.js
@@ -0,0 +1,27 @@
+const GET_INMOBILIARIAS = "GET_INMOBILIARIAS";
+const GET_INMOBILIARIAS_SUCCESS = "GET_INMOBILIARIAS_SUCCESS";
+const GET_INMOBILIARIAS_FAILURE = "GET_INMOBILIARIAS_FAILURE";
+
+const getInmobiliarias = () => {
+  return (dispatch) => {
+    //nameless functions
+    // Initial action dispatched
+    dispatch({ type: GET_INMOBILIARIAS });
+    // Return promise with success and failure actions
+    return axios.get("/api/auth/user").then(
+      (inmobiliarias) =>
+        dispatch({ type: GET_INMOBILIARIAS_SUCCESS, payload: inmobiliarias }),
+      (err) => dispatch({ type: GET_INMOBILIARIAS_FAILURE, payload: err })
+    );
+  };
+};
+
+const types = {
+  GET_INMOBILIARIAS,
+  GET_INMOBILIARIAS_SUCCESS,
+  GET_INMOBILIARIAS_FAILURE,
+};
+
+const actions = { getInmobiliarias };
+
+export { types, actions };
