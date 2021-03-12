4e0ec178ad94ab66ba9e0875fff877c1ba04f631
Faro08 <76972056+Faro08@users.noreply.github.com>
2021-03-12 19:26:52 -0300

    bckp header mio antes de bajar el de edu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,99 @@
 import React from "react";
-import AdminModal from "../components/admin/AdminModal";
+import styled from "styled-components";
 
-const Home = () => {
-  return (
-    <div>
-      <AdminModal></AdminModal>
-    </div>
+import Logo from "../images/logoproper.png";
+
+const Header = styled.header`
+  background: pink;
+ /*  height: 100px; */
+  display: flex;
+  
+`;
+
+const Header__Logo = styled.div`
+  width:50%;
+  background:red;
+  display:flex;
+  justify-content: flex-start;
+  align-items:center;
+  div{
+    margin-left: 50px;
+    width: 40%;
+    /* height:80%; */
+    /* display: flex;
+    align-items: center; */
+    background: green;
+    display: flex;
+    align-items: center;
+
+    img{
+      
+      width:100%;
+      height: auto;
+      background: yellow;
+    }
+  }
+  
+`;
+
+const Header__Menu = styled.div`
+  width:50%;
+  background: lightblue;
+  nav{
+    ul{
+      list-style-type: none;
+      display:flex;
+      flex-direction: row;
+      
+      li{
+        a{
+          text-decoration: none;
+          color: black;
+          
+          :hover{
+            background: orange;
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Home__Hero = styled.div`
+
+`;
+
+function Home(){
+  return(
+    <React.Fragment>
+      <Header>
+        <Header__Logo>
+          <div>
+            <img src={Logo} alt= "Logo"/>
+          </div>
+        </Header__Logo>
+        <Header__Menu>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">NOSOTROS</a>
+              </li>
+              <li>
+                <a href="#">PROYECTOS</a>
+              </li>
+              <li>
+                <a href="#">PRECIOS</a>
+              </li>
+              <li>
+                <a href="#">CONTACTO</a>
+              </li>
+            </ul>
+          </nav>
+        </Header__Menu>
+      </Header>
+      <Home__Hero></Home__Hero>
+    </React.Fragment>
   );
-};
+}
 
 export default Home;
