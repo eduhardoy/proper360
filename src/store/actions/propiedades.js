ee8398d373e60e445e219a70420b145ab51d4f7b
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-04-19 11:54:00 -0300

    List of accordions

--- src/store/actions/propiedades.js
+++ src/store/actions/propiedades.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://190.106.132.208:3006/" })
+const api = axios.create({ baseURL: "http://198.58.123.120:3006/" })
 
 //Types
 const GET_PROPIEDADES = "GET_PROPIEDADES"
@@ -15,14 +15,16 @@ const types = {
 
 
 const getPropiedades = () => {
-  return dispatch => {
-    dispatch({ type: GET_PROPIEDADES })
-
-    return axios.get("propiedades")
+  return (dispatch) => {
+    //nameless functions
+    // Initial action dispatched
+    dispatch({ type: GET_PROPIEDADES });
+    // Return promise with success and failure actions
+    return api.get("propiedades")
       .then(propiedades => dispatch({ type: GET_PROPIEDADES_SUCCESS, payload: propiedades.data }))
-      .catch(err => dispatch({ tyoe: GET_PROPIEDADES_FAILUTE, payload: err.message }))
+      .catch(err => dispatch({ type: GET_PROPIEDADES_FAILUTE, payload: err.message }))
   }
-}
+};
 
 
 const actions = { getPropiedades };
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import inmobiliarias from "./inmobiliarias";
 import propiedades from './propiedades'
+import clientes from './clientes'
 
 const rootReducers = combineReducers({
   inmobiliarias: inmobiliarias,
-  propiedades: propiedades
+  propiedades: propiedades,
+  clientes: clientes
 });
 
 export default rootReducers;
