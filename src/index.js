f3a02f70571d3a9f58ac8d41b4c6239ec1299a6a
Edu Hardoy <eduhardoy@gmail.com>
2021-05-05 10:49:26 -0300

    React-ga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { GlobalStyle } from "./globalStyles";
+import ReactGA from "react-ga";
 
 //REDUX
 import { Provider } from "react-redux";
@@ -9,6 +10,11 @@ import store from "./store";
 import Routes from "./routes";
 
 const App = () => {
+  React.useEffect(() => {
+    ReactGA.initialize('G-TQNB5J3LQ0')
+    ReactGA.pageview('/')
+  }, [])
+
   return (
     <div>
       <GlobalStyle />
